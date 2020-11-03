817e9975e00c16d1ad321cad376a2cc4a45ec913
Yoelvis Oliveros <yoelvisoliveros@gmail.com>
2020-11-03 10:43:02 -0400

    fit page modification, styling and btn text

--- src/pages/Fit.tsx
+++ src/pages/Fit.tsx
@@ -6,18 +6,23 @@ const Fit = () =>
 (
 	<Layout>
 		<SEO title='FIT' />
-		<section style={{ marginTop: '10vh' }} >
+		<section style={{ position: 'relative', height: '70vh' }} >
 			<a style={{
 				padding: '2rem',
 				backgroundColor: '#663399',
-				borderRadius: '25%',
+				borderRadius: '2%',
 				textDecoration: 'none',
 				color: '#FFF',
-				fontSize: '18px'
+				fontSize: '18px',
+				margin: 'auto',
+				position: 'absolute',
+				top: '50%',
+				left: '50%',
+				transform: 'translate(-50%, -50%)'
 			}}
 				href="https://github.com/yoliveros/ontouch-site/raw/master/src/FitInstaller/FitInstaller.msi"
 				download>
-				Descargar FIT
+				Descargar FIT: Version de Prueba
 			</a>
 		</section>
 	</Layout>
